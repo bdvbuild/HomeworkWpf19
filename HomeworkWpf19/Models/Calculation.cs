a53ecf2eb5f5b0ca0fad6b71d6d5838496ddcdf2
Dmitriy Baranov <bdvbuild@gmail.com>
2023-01-16 21:13:24 +0400

    Добавьте файлы проекта.

---
+++ HomeworkWpf19/Models/Calculation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkWpf19.Models
+{
+    static class Calculation
+    {
+        public static double Calc(double x) => 2 * x * Math.PI;
+    }
+}
---
+++ HomeworkWpf19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using HomeworkWpf19.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HomeworkWpf19.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+        private double lenght;
+        public double Lenght
+        {
+            get => lenght;
+            set
+            {
+                lenght = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand CalcCommand { get; }
+        private void OnCalcCommandExecute(object p)
+        {
+            Lenght = Calculation.Calc(Radius);
+        }
+        private bool CanCalcCommandExecuted(object p)
+        {
+            return true;
+        }
+        public MainWindowViewModel()
+        {
+            CalcCommand = new RelayCommand(OnCalcCommandExecute, CanCalcCommandExecuted);
+        }
+
+    }
+}
